6edb3a658cb0e88ace22a70a33b1614a31383f84
Klayver Ximenes <klayverx@gmail.com>
2021-06-19 19:14:52 -0300

    commit

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React, { useState } from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [color, setColor] = useState('#f0f')
+
+	function changeColor() {
+		const randomColor = Math.floor(Math.random() * 16777215).toString(16)
+		setColor(`#${randomColor}`)
+	}
+
+	return (
+		<View style={styles.container} backgroundColor={color}>
+			<Text>Hello!</Text>
+			<Text>Click on this button to change background color! XD</Text>
+			<Button title="Change" onPress={() => changeColor()} color="black" />
+
+			<Text>This color is {color}</Text>
+			<StatusBar style="auto" />
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+})
